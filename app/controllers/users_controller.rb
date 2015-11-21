9aa64a007614bfd1860ba05f1bb9008baa22a10c
Chen Yang <yangc5@mail.uc.edu>
2015-11-20 19:29:50 -0600

    added username to sign up form and route to dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120234515) do
+ActiveRecord::Schema.define(version: 20151121010820) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["username"], name: "index_users_on_username"
 
 end
